34f27a427b61f4d4a511aa1521f129c89c7815c3
Henry Mao <admin@calclavia.com>
2017-01-19 10:04:14 -0800

    Added Javascript

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,17 +2,20 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
-	});
+    $("#testjs").click(function(e) {
+        $('.jumbotron h1').text("Javascript is connected");
+    });
 
-	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+    // Add any additional listeners here
+    $("a.thumbnail").click(function(e) {
+        e.preventDefault();
+        $(this).css("background-color", "#7fff00")
+    });
 }
